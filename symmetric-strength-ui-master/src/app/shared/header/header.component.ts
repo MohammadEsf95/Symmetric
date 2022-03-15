68dffa5ee6ad95fe127f543e7f1c6206efaf882b
mh.esfahani <esfahanimh95@gmail.com>
2022-03-15 15:55:40 +0330

    feat: header initialized

---
+++ symmetric-strength-ui-master/src/app/shared/header/header.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [{
+      label: 'Home',
+      // items: [{
+      //   label: 'New',
+      //   icon: 'pi pi-fw pi-plus',
+      //   items: [
+      //     {label: 'Project'},
+      //     {label: 'Other'},
+      //   ]
+      // },
+      //   {label: 'Open'},
+      //   {label: 'Quit'}
+      // ]
+    },
+      {
+        label: 'Strength Standards',
+        // items: [
+        //   {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+        //   {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+        // ]
+      },
+      {
+        label: 'Calculators',
+        items: [
+          {label: '1RM Calculator'},
+          {label: 'Wilks Calculator'},
+          {label: 'TDEE Calculator'},
+          {label: 'Ideal Bodyweight Calculator'},
+        ]
+      },
+      {
+        label: 'About'
+      }]
+  }
+
+  ngOnInit(): void {
+  }
+}
--- symmetric-strength-ui-master/src/app/shared/shared.module.ts
+++ symmetric-strength-ui-master/src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import {NgModule} from "@angular/core";
 import {ButtonModule} from "primeng/button";
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {MegaMenuModule} from "primeng/megamenu";
+import {MenubarModule} from "primeng/menubar";
+import {InputTextModule} from "primeng/inputtext";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule(
   {
-    declarations:[],
-    exports:[],
-    imports:[
+    declarations:[
+    HeaderComponent,
+    FooterComponent
+  ],
+      exports: [
+          HeaderComponent
+      ],
+    imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
       ButtonModule,
+      MegaMenuModule,
+      MenubarModule,
+      InputTextModule,
     ]
   }
 )
