236e385382e5a9638a15d522fe34e6ffba5b5a35
Mohammad <esfahanimh95@gmail.com>
2022-03-19 19:57:31 +0330

    wip: strength standards initialized

---
+++ symmetric-strength-ui-master/src/app/strength-standards/strength-standards/strength-standards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strength-standards',
+  templateUrl: './strength-standards.component.html',
+  styleUrls: ['./strength-standards.component.css']
+})
+export class StrengthStandardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
