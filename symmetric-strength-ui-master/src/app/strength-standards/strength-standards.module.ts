00a395569d71078d05924e5bc1240d2c5b45ca67
Mohammad <esfahanimh95@gmail.com>
2022-03-19 21:25:04 +0330

    wip: strength standard done

--- symmetric-strength-ui-master/src/app/strength-standards/strength-standards.module.ts
+++ symmetric-strength-ui-master/src/app/strength-standards/strength-standards.module.ts
@@ -2,14 +2,24 @@ import {NgModule} from "@angular/core";
 import {StrengthStandardsComponent} from "./strength-standards/strength-standards.component";
 import {SharedModule} from "../shared/shared.module";
 import {StrengthStandardsRoutingModule} from "./strength-standards-routing.module";
+import {DropdownModule} from "primeng/dropdown";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {InputNumberModule} from "primeng/inputnumber";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations:[
     StrengthStandardsComponent
   ],
   imports:[
+    DropdownModule,
+    InputNumberModule,
     StrengthStandardsRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    CommonModule,
+    ButtonModule
   ],
   providers:[]
 })
