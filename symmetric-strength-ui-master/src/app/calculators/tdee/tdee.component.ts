83cfdafd2a16fba0e0161e5191e977fcc61f6954
Mohammad <esfahanimh95@gmail.com>
2022-03-21 22:20:09 +0330

    wip: tdee first part done

--- symmetric-strength-ui-master/src/app/calculators/tdee/tdee.component.ts
+++ symmetric-strength-ui-master/src/app/calculators/tdee/tdee.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TdeeComponent implements OnInit {
 
-  constructor() { }
+  units: any[] = [];
+  activityLevels: any[] = [];
+  selectedActivity: any = {};
+  selectedUnit: any = {};
+  selectedGender: any = {};
+  genders: any[] = [{type: 'Male'}, {type: 'Female'}];
+  age: any;
+  bodyWeight: any;
+  bodyFat: any;
+  feet: any;
+  inches: any;
+
+  constructor() {
+    this.units = [
+      {name: 'Metric', label: 'KG'},
+      {name: 'Imperial', label: 'LBS'}
+    ]
+
+
+    this.activityLevels = [
+      {name: 'Inactive', label: 'Extremely inactive (bed-ridden)'},
+      {name: 'Sedentary', label: 'Sedentary (office worker, little or no exercise)'},
+      {name: 'Average', label: 'Average (some exercise throughout the week)'},
+      {name: 'Moderately active', label: 'Moderately active (physical job or an hour of exercise daily)'},
+      {name: 'Vigorously active', label: 'Vigorously active (very physical job or two hours of exercise daily)'},
+      {name: 'Extremely active', label: 'Extremely active (competitive endurance athlete)'},
+    ]
+  }
 
   ngOnInit(): void {
+    this.selectedUnit = this.units[0];
+    this.selectedGender = this.genders[0];
+    this.selectedActivity = this.activityLevels[2];
   }
 
 }
