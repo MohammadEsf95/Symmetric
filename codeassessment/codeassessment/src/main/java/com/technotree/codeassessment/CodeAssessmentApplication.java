a96108652af834b58a9924058854693ad867ed89
mh.esfahani <esfahanimh95@gmail.com>
2022-04-17 13:36:50 +0430

    feat: aspect added for logging

--- codeassessment/codeassessment/src/main/java/com/technotree/codeassessment/CodeAssessmentApplication.java
+++ codeassessment/codeassessment/src/main/java/com/technotree/codeassessment/CodeAssessmentApplication.java
@@ -2,8 +2,10 @@ package com.technotree.codeassessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CodeAssessmentApplication {
 
 	public static void main(String[] args) {
---
+++ codeassessment/codeassessment/src/main/java/com/technotree/codeassessment/infrastructure/aspect/LoggingAspect.java
@@ -0,0 +1,103 @@
+package com.technotree.codeassessment.infrastructure.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+//    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+//
+//    @Around("execution(* com.technotree.codeassessment.application..*(..)))")
+//    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+//    {
+//        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+//
+//        //Get intercepted method details
+//        String className = methodSignature.getDeclaringType().getSimpleName();
+//        String methodName = methodSignature.getName();
+//
+//        final StopWatch stopWatch = new StopWatch();
+//
+//        //Measure method execution time
+//        stopWatch.start();
+//        Object result = proceedingJoinPoint.proceed();
+//        stopWatch.stop();
+//
+//        //Log method execution time
+//        LOGGER.info("Execution time of " + className + "." + methodName + " :: " + stopWatch.getTotalTimeMillis() + " ms");
+//
+//        return result;
+//    }
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Pointcut that matches all repositories, services and Web REST endpoints.
+     */
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    /**
+     * Pointcut that matches all Spring beans in the application's main packages.
+     */
+    @Pointcut("within(com.technotree.codeassessment.application..*)" +
+            " || within(com.technotree.codeassessment.presentation..*)" +
+            " || within(com.technotree.codeassessment.domain..*)")
+    public void applicationPackagePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    /**
+     * Advice that logs methods throwing exceptions.
+     *
+     * @param joinPoint join point for advice
+     * @param e exception
+     */
+    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+    /**
+     * Advice that logs when a method is entered and exited.
+     *
+     * @param joinPoint join point for advice
+     * @return result
+     * @throws Throwable throws IllegalArgumentException
+     */
+    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(), result);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
+}
